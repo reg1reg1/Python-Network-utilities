27f4af575f879862d602e748fca6041fc88fc3f4
Yuvraj Singh <yuvraj.singh19@gmail.com>
2023-09-04 15:13:17 -0400

    Update Invoke-Syscommand.ps1
    Changes to DotNet Remoting

--- Powershell/Remoting and Dotnet/Invoke-Syscommand.ps1
+++ Powershell/Remoting and Dotnet/Invoke-Syscommand.ps1
@@ -5,7 +5,7 @@ $DotNetCSharp = @"
         {
             System.Diagnostics.Process.Start("taskmgr.exe");
         }
-        public void netuser (string cmd)
+        public static void netuser (string cmd)
         {
             string clistr = "/k net.exe "+cmd;
             System.Diagnostics.Process.Start("cmd.exe", clistr);
@@ -15,19 +15,23 @@ $DotNetCSharp = @"
             System.Diagnostics.Process.Start("nslookup.exe",domainname);
 
         }
+        public void regeditStart()
+        {
+            System.Diagnostics.Process.Start("regedit.exe");
+        }
 
     }
 "@
 
-
+#Note that once this has been added, the session will persist it even after the script has exited, and will throw an error. So close the powershell session and rerun the script.
 Add-Type -TypeDefinition $DotNetCSharp
-[Syscommands]::startTaskMgr()
+#[Syscommands]::startTaskMgr()
 
 
-[Syscommands]::nsCheck("google.com")
+#[Syscommands]::nsCheck("google.com")
 
 
 #Alternatively
-
-$var1 = New-Object Syscommands
-$var1.netuser("user")
+#Methods must be static to be accessed via this syntax
+$obj = New-Object Syscommands
+$obj.regeditStart()
